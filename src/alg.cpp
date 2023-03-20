56b4ac8aa47fca42464eff1d457af0a54dd3e5c7
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-03-20 21:32:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch(int* arr, int size, int value) {
+    int left = 0;
+    int right = size - 1;
+    int f = 0;
+    while (left <= right) {
+        int ser = (right + left) / 2;
+        if (*(arr + ser) == value) {
+            f++;
+            int newser = ser;
+            while ((*(arr + ser + 1)) == value) {
+                f++;
+                ser++;
+            }
+            while ((*(arr + newser - 1)) == value) {
+                f++;
+                newser--;
+            }
+            return f;
+        } else if (*(arr + ser) > value) {
+            right = ser - 1;
+        } else {
+            left = ser + 1;
+        }
+    }
+    return f;
 }
